9cee494b4f74e93176883ee55ebe8e3264b22f8b
wangjunle <1016687804@qq.com>
2026-07-10 11:44:51 +0800

    uiq

---
+++ src/data/beneficiary-templates.ts
@@ -0,0 +1,10 @@
+export const THAI_BANKS = [
+  { code: "002", name: "Bangkok Bank" },
+  { code: "004", name: "Kasikorn Bank" },
+  { code: "006", name: "Krung Thai Bank" },
+  { code: "014", name: "Siam Commercial Bank" },
+];
+
+export const WALLET_BRANDS: Record<string, string[]> = {
+  QRIS: ["DANA", "OVO", "GoPay", "ShopeePay"],
+};
